0fa7294ec5ab0e0fa6a32a5911a70639fd32f9df
Max Heisinger <mail@maximaximal.com>
2016-07-13 14:56:03 +0200

    Add a config file and cmake compatability.

---
+++ common/src/config.c
@@ -0,0 +1,18 @@
+#include <piga/config.h>
+
+int piga_get_version_major()
+{
+    return PIGA_VERSION_MAJOR;
+}
+int piga_get_version_minor()
+{
+    return PIGA_VERSION_MINOR;
+}
+int piga_get_version_patch()
+{
+    return PIGA_VERSION_PATCH;
+}
+int piga_get_version_tweak()
+{
+    return PIGA_VERSION_TWEAK;
+}
